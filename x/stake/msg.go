jlandrews/cosmos-sdk

Fixed json field names in stake msg types

x/stake/msg.go
@@ -117,8 +117,8 @@ func (msg MsgEditCandidacy) ValidateBasic() sdk.Error {
 
 // MsgDelegate - struct for bonding transactions
 type MsgDelegate struct {
-	DelegatorAddr sdk.Address `json:"address"`
-	CandidateAddr sdk.Address `json:"address"`
+	DelegatorAddr sdk.Address `json:"delegator"`
+	CandidateAddr sdk.Address `json:"candidate"`
 	Bond          sdk.Coin    `json:"bond"`
 }
 
@@ -164,8 +164,8 @@ func (msg MsgDelegate) ValidateBasic() sdk.Error {
 
 // MsgUnbond - struct for unbonding transactions
 type MsgUnbond struct {
-	DelegatorAddr sdk.Address `json:"address"`
-	CandidateAddr sdk.Address `json:"address"`
+	DelegatorAddr sdk.Address `json:"delegator"`
+	CandidateAddr sdk.Address `json:"candidate"`
 	Shares        string      `json:"shares"`
 }
 

togetherAll7/greenfield-cosmos-sdk

x/stake/msg.go
@@ -117,8 +117,8 @@ func (msg MsgEditCandidacy) ValidateBasic() sdk.Error {
 
 // MsgDelegate - struct for bonding transactions
 type MsgDelegate struct {
-	DelegatorAddr sdk.Address `json:"address"`
-	ValidatorAddr sdk.Address `json:"address"`
+	DelegatorAddr sdk.Address `json:"delegator"`
+	ValidatorAddr sdk.Address `json:"candidate"`
 	Bond          sdk.Coin    `json:"bond"`
 }
 
@@ -164,8 +164,8 @@ func (msg MsgDelegate) ValidateBasic() sdk.Error {
 
 // MsgUnbond - struct for unbonding transactions
 type MsgUnbond struct {
-	DelegatorAddr sdk.Address `json:"address"`
-	ValidatorAddr sdk.Address `json:"address"`
+	DelegatorAddr sdk.Address `json:"delegator"`
+	ValidatorAddr sdk.Address `json:"candidate"`
 	Shares        string      `json:"shares"`
 }
 