DonWono/cosmos-sdk

Reintroduce collect-gentxs's --gentx-dir flag
It went lost in last genesis workflow refactoring.

Also address structs initializations as per #2835.

cmd/gaia/init/collect.go
@@ -17,6 +17,10 @@ import (
 	"github.com/tendermint/tendermint/types"
 )
 
+const (
+	flagGenTxDir = "gentx-dir"
+)
+
 type initConfig struct {
 	ChainID   string
 	GenTxsDir string
@@ -34,7 +38,6 @@ func CollectGenTxsCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 			config := ctx.Config
 			config.SetRoot(viper.GetString(cli.HomeFlag))
 			name := viper.GetString(client.FlagName)
-
 			nodeID, valPubKey, err := InitializeNodeValidatorFiles(config)
 			if err != nil {
 				return err
@@ -45,19 +48,13 @@ func CollectGenTxsCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 				return err
 			}
 
-			toPrint := printInfo{
-				Moniker: config.Moniker,
-				ChainID: genDoc.ChainID,
-				NodeID:  nodeID,
+			genTxsDir := viper.GetString(flagGenTxDir)
+			if genTxsDir == "" {
+				genTxsDir = filepath.Join(config.RootDir, "config", "gentx")
 			}
 
-			initCfg := initConfig{
-				ChainID:   genDoc.ChainID,
-				GenTxsDir: filepath.Join(config.RootDir, "config", "gentx"),
-				Name:      name,
-				NodeID:    nodeID,
-				ValPubKey: valPubKey,
-			}
+			toPrint := makePrintInfo(config.Moniker, genDoc.ChainID, nodeID, genTxsDir, json.RawMessage(""))
+			initCfg := makeInitConfig(genDoc.ChainID, genTxsDir, name, nodeID, valPubKey)
 
 			appMessage, err := genAppStateFromConfig(cdc, config, initCfg, genDoc)
 			if err != nil {
@@ -72,6 +69,9 @@ func CollectGenTxsCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 	}
 
 	cmd.Flags().String(cli.HomeFlag, app.DefaultNodeHome, "node's home directory")
+	cmd.Flags().String(flagGenTxDir, "",
+		"override default \"gentx\" directory from which collect and execute "+
+			"genesis transactions; default [--home]/config/gentx/")
 	return cmd
 }
 
@@ -115,4 +115,28 @@ func genAppStateFromConfig(
 
 	err = ExportGenesisFile(genFile, initCfg.ChainID, nil, appState)
 	return
+}
+
+func makeInitConfig(chainID, genTxsDir, name, nodeID string,
+	valPubKey crypto.PubKey) initConfig {
+
+	return initConfig{
+		ChainID:   chainID,
+		GenTxsDir: genTxsDir,
+		Name:      name,
+		NodeID:    nodeID,
+		ValPubKey: valPubKey,
+	}
+}
+
+func makePrintInfo(moniker, chainID, nodeID, genTxsDir string,
+	appMessage json.RawMessage) printInfo {
+
+	return printInfo{
+		Moniker:    moniker,
+		ChainID:    chainID,
+		NodeID:     nodeID,
+		GenTxsDir:  genTxsDir,
+		AppMessage: appMessage,
+	}
 }

cmd/gaia/init/init.go
@@ -27,6 +27,7 @@ type printInfo struct {
 	Moniker    string          `json:"moniker"`
 	ChainID    string          `json:"chain_id"`
 	NodeID     string          `json:"node_id"`
+	GenTxsDir  string          `json:"gentxs_dir"`
 	AppMessage json.RawMessage `json:"app_message"`
 }
 
@@ -76,12 +77,7 @@ func InitCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 				return err
 			}
 
-			toPrint := printInfo{
-				ChainID:    chainID,
-				Moniker:    config.Moniker,
-				NodeID:     nodeID,
-				AppMessage: appState,
-			}
+			toPrint := makePrintInfo(config.Moniker, chainID, nodeID, "", appState)
 
 			cfg.WriteConfigFile(filepath.Join(config.RootDir, "config", "config.toml"), config)
 

cmd/gaia/init/testnet.go
@@ -278,13 +278,7 @@ func collectGenFiles(
 		config.SetRoot(nodeDir)
 
 		nodeID, valPubKey := nodeIDs[i], valPubKeys[i]
-		initCfg := initConfig{
-			ChainID:   chainID,
-			GenTxsDir: gentxsDir,
-			Name:      moniker,
-			NodeID:    nodeID,
-			ValPubKey: valPubKey,
-		}
+		initCfg := makeInitConfig(chainID, gentxsDir, moniker, nodeID, valPubKey)
 
 		genDoc, err := loadGenesisDoc(cdc, config.GenesisFile())
 		if err != nil {

togetherAll7/greenfield-cosmos-sdk

cmd/gaia/init/collect.go
@@ -18,6 +18,10 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/auth"
 )
 
+const (
+	flagGenTxDir = "gentx-dir"
+)
+
 type initConfig struct {
 	ChainID   string
 	GenTxsDir string
@@ -35,7 +39,6 @@ func CollectGenTxsCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 			config := ctx.Config
 			config.SetRoot(viper.GetString(cli.HomeFlag))
 			name := viper.GetString(client.FlagName)
-
 			nodeID, valPubKey, err := InitializeNodeValidatorFiles(config)
 			if err != nil {
 				return err
@@ -46,19 +49,13 @@ func CollectGenTxsCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 				return err
 			}
 
-			toPrint := printInfo{
-				Moniker: config.Moniker,
-				ChainID: genDoc.ChainID,
-				NodeID:  nodeID,
+			genTxsDir := viper.GetString(flagGenTxDir)
+			if genTxsDir == "" {
+				genTxsDir = filepath.Join(config.RootDir, "config", "gentx")
 			}
 
-			initCfg := initConfig{
-				ChainID:   genDoc.ChainID,
-				GenTxsDir: filepath.Join(config.RootDir, "config", "gentx"),
-				Name:      name,
-				NodeID:    nodeID,
-				ValPubKey: valPubKey,
-			}
+			toPrint := newPrintInfo(config.Moniker, genDoc.ChainID, nodeID, genTxsDir, json.RawMessage(""))
+			initCfg := newInitConfig(genDoc.ChainID, genTxsDir, name, nodeID, valPubKey)
 
 			appMessage, err := genAppStateFromConfig(cdc, config, initCfg, genDoc)
 			if err != nil {
@@ -73,6 +70,9 @@ func CollectGenTxsCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 	}
 
 	cmd.Flags().String(cli.HomeFlag, app.DefaultNodeHome, "node's home directory")
+	cmd.Flags().String(flagGenTxDir, "",
+		"override default \"gentx\" directory from which collect and execute "+
+			"genesis transactions; default [--home]/config/gentx/")
 	return cmd
 }
 
@@ -116,4 +116,28 @@ func genAppStateFromConfig(
 
 	err = ExportGenesisFile(genFile, initCfg.ChainID, nil, appState)
 	return
+}
+
+func newInitConfig(chainID, genTxsDir, name, nodeID string,
+	valPubKey crypto.PubKey) initConfig {
+
+	return initConfig{
+		ChainID:   chainID,
+		GenTxsDir: genTxsDir,
+		Name:      name,
+		NodeID:    nodeID,
+		ValPubKey: valPubKey,
+	}
+}
+
+func newPrintInfo(moniker, chainID, nodeID, genTxsDir string,
+	appMessage json.RawMessage) printInfo {
+
+	return printInfo{
+		Moniker:    moniker,
+		ChainID:    chainID,
+		NodeID:     nodeID,
+		GenTxsDir:  genTxsDir,
+		AppMessage: appMessage,
+	}
 }

cmd/gaia/init/init.go
@@ -28,6 +28,7 @@ type printInfo struct {
 	Moniker    string          `json:"moniker"`
 	ChainID    string          `json:"chain_id"`
 	NodeID     string          `json:"node_id"`
+	GenTxsDir  string          `json:"gentxs_dir"`
 	AppMessage json.RawMessage `json:"app_message"`
 }
 
@@ -77,12 +78,7 @@ func InitCmd(ctx *server.Context, cdc *codec.Codec) *cobra.Command {
 				return err
 			}
 
-			toPrint := printInfo{
-				ChainID:    chainID,
-				Moniker:    config.Moniker,
-				NodeID:     nodeID,
-				AppMessage: appState,
-			}
+			toPrint := newPrintInfo(config.Moniker, chainID, nodeID, "", appState)
 
 			cfg.WriteConfigFile(filepath.Join(config.RootDir, "config", "config.toml"), config)
 

cmd/gaia/init/testnet.go
@@ -279,13 +279,7 @@ func collectGenFiles(
 		config.SetRoot(nodeDir)
 
 		nodeID, valPubKey := nodeIDs[i], valPubKeys[i]
-		initCfg := initConfig{
-			ChainID:   chainID,
-			GenTxsDir: gentxsDir,
-			Name:      moniker,
-			NodeID:    nodeID,
-			ValPubKey: valPubKey,
-		}
+		initCfg := newInitConfig(chainID, gentxsDir, moniker, nodeID, valPubKey)
 
 		genDoc, err := loadGenesisDoc(cdc, config.GenesisFile())
 		if err != nil {